97170e656dc0529cdf1ec9a13c669be4e476194a
Nhut Nguyen <nhut.nguyen@saigontechnology.com>
2021-10-26 23:58:18 +0700

    [FE-0001] Finished designing the banner and menu bar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,9 +5,9 @@ import { HomeComponent } from './components/home/home.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
-  { path: 'home', component: HomeComponent},
-  { path: 'products', component: ProductComponent},
-  { path: "**", redirectTo:"home"}
+  { path: 'home', component: HomeComponent },
+  { path: 'products', component: ProductComponent },
+  { path: "**", redirectTo:"home" }
 ]
 
 @NgModule({
